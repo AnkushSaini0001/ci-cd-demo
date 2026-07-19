fce6bacec0d632d84fa160bc2c721155da9b199c
Ankush Saini <Ankush@ANKUSH-SAINI.altruistindia.com>
2026-07-19 14:19:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>Hello Docker + React!</h1>
+      <p>Dummy project.</p>
+    </div>
+  );
+}
