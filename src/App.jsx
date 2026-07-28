e1016a68b2150ef7685d288618a2450e7015c2d8
Ankush Saini <Ankush@ANKUSH-SAINI.altruistindia.com>
2026-07-28 17:38:18 +0530

    changes ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ fontFamily: "Arial", padding: "40px" }}>
       <h1>Hello Docker + React!</h1>
-      <p>CI/CD</p>
+      <p>CI/CD pipe line testing</p>
     </div>
   );
 }
